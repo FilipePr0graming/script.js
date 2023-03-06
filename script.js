bc0e74f0aa615b4ff1770ce29cc9c3a7160bf0fc
FilipePr0graming <127054318+FilipePr0graming@users.noreply.github.com>
2023-03-06 00:45:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-function countDown() {
+// Set the date and time to count down to
+var countDownDate = new Date("Mar 14, 2023 23:59:59").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get the current date and time
   var now = new Date().getTime();
-  var countDate = new Date("March 31, 2023 23:59:59").getTime();
-  var distance = countDate - now;
-  
+
+  // Calculate the time remaining
+  var distance = countDownDate - now;
+
+  // Calculate the days, hours, minutes, and seconds remaining
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-  
-  if (distance < 0) {
-    clearInterval(countDownInterval);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}
 
-var countDownInterval = setInterval(countDown, 1000);
+  // Display the countdown in the element with id="countdown"
+  document.getElementById("countdown").innerHTML = "Faltam " + days + " dias, " + hours + " horas, " + minutes + " minutos e " + seconds + " segundos para acabar a promoção!";
+
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "A promoção acabou!";
+  }
+}, 1000);
